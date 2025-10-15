c017c2e0892516be50f1e44cc1cd51451f02c0e5
christopher son lopez <christophersonlopez@gmail.com>
2025-10-15 15:35:34 -0400

    added a function method to display my assignment

---
+++ Function1.js
@@ -0,0 +1,7 @@
+ 
+
+     function Myfunction(){
+            var name="this is my second assignment";
+            var result1=name.fontcolor("green");
+            document.getElementById("Christ").innerHTML=result1;
+        }
